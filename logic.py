e1953c986f627e18b1779dff4aa5c81a6904cbb0
en.kuo <en.kuo@istayreal.com>
2017-07-07 16:29:14 +0800

    fix desc

--- logic.py
+++ logic.py
@@ -7,13 +7,15 @@ def dayoff_create(user, text, config):
     if len(times) < 6:
         return "錯誤的時間資訊：請輸入日期與時間範圍\n" \
                "範例：2017 0101 0800 2017 0101 1200 請假理由"
-    print(times)
-    desc = '{}請假 {}-{}-{}~{}-{}-{}'.format(
+    desc_head = '{}請假'.format(user)
+    desc_end = '{}-{}-{}~{}-{}-{}'.format(
         user, times[0], times[1], times[2], times[3], times[4], times[5])
     if len(times) >= 7:
         reason = times[6]
+        desc = desc_head + reason + desc_end
     else:
         reason = '{}請假'.format(user)
+        desc = desc_head + desc_end
     # TODO 請假
     service = Service.get_service(config, user)
     event = {
