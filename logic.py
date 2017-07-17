273513a9bbe1aa8b6be5a79c581515805e4d61c1
en.kuo <en.kuo@istayreal.com>
2017-07-17 11:54:34 +0800

    edit message

--- logic.py
+++ logic.py
@@ -7,18 +7,25 @@ def dayoff_create(user, text, config):
     if len(times) < 6:
         return "錯誤的時間資訊：請輸入日期與時間範圍\n" \
                "範例：2017 0101 0800 2017 0101 1200 請假理由"
-    desc_head = '{}請假'.format(user)
-    desc_end = '{}-{}-{}~{}-{}-{}'.format(
-        user, times[0], times[1], times[2], times[3], times[4], times[5])
-    if len(times) >= 7:
-        reason = times[6]
-        desc = desc_head + reason + desc_end
-    else:
-        reason = '{}請假'.format(user)
-        desc = desc_head + desc_end
+    desc, reason = make_day0ff_result(user, times)
     # TODO 請假
     service = Service.get_service(config, user)
-    event = {
+    event = make_dayoff_event(reason, desc, times)
+
+    created_event = service.events().insert(
+        calendarId=config.get('CALENDAR_ID'),  # config.get('CALENDAR_ID'),
+        body=event
+    ).execute()
+
+    result = {
+        "response_type": "in_channel",
+        "text": desc,
+    }
+    return jsonify(result)
+
+
+def make_dayoff_event(reason, desc, times):
+    return {
         'summary': reason,
         'description': desc,
         'start': {
@@ -34,15 +41,46 @@ def dayoff_create(user, text, config):
             'timeZone': 'Asia/Taipei'
         }
     }
-    created_event = service.events().insert(
-        calendarId=config.get('CALENDAR_ID'),  # config.get('CALENDAR_ID'),
-        body=event
-    ).execute()
-    result = {
-        "response_type": "in_channel",
-        "text": desc,
-    }
-    return jsonify(result)
+
+
+def make_day0ff_result(user, times):
+    year_a = times[0]
+    day_a = times[1]
+    hour_a = times[2]
+    year_b = times[3]
+    day_b = times[4]
+    hour_b = times[5]
+    same_year = False
+    same_month = False
+    same_day = False
+    if year_a == year_b:
+        same_year = True
+    if day_a[:2] == day_b[2:]:
+        same_month = True
+    if day_a == day_b:
+        same_day = True
+    # 有填請假理由
+    if len(times) == 7:
+        reason = times[6]
+    else:
+        reason = '{}請假'.format(user)
+
+    if same_year and same_month and same_day:
+        time_desc = '@{}-{}-{} from {}:{} to {}:{}'.format(
+            year_a, day_a[:2], day_a[2:],
+            hour_a[:2], hour_a[2:], hour_b[:2], hour_b[2:])
+    elif same_year and not same_day:
+        time_desc = '@{} from {}-{} {}:{} to {}-{} {}:{}'.format(
+            year_a, day_a[:2], day_a[2:], hour_a[:2], hour_a[2:],
+            day_b[:2], day_b[2:], hour_b[:2], hour_b[2:]
+        )
+    elif not same_year:
+        time_desc = 'from {}-{}-{} {}:{} to {}-{}-{} {}:{}'.format(
+            year_a, day_a[:2], day_a[2:], hour_a[:2], hour_a[2:],
+            year_b, day_b[:2], day_b[2:], hour_b[:2], hour_b[2:]
+        )
+    desc = reason + ' {}'.format(time_desc)
+    return desc, reason
 
 
 def dayoff_list(config):
